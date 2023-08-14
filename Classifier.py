3a62dde0124f3b313e378d685c1e23e0f4a8ba1a
gnthr-solve <nomad.gnthr@gmail.com>
2023-08-14 10:35:03 +0200

    Papers added and metrics in collections updated

---
+++ Classifier.py
@@ -0,0 +1,36 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+class Classifier(BaseEstimator, ClassifierMixin):
+    
+    def __init__(self, classifier=None):
+        self.classifier = classifier
+
+    def fit(self, X, y):
+
+        if self.classifier is None:
+            raise ValueError("Classifier is not provided. Please initialize the classifier.")
+        
+        self.classifier.fit(X, y)
+        return self
+
+    def predict(self, X):
+
+        if self.classifier is None:
+            raise ValueError("Classifier is not provided. Please initialize the classifier.")
+        
+        return self.classifier.predict(X)
+
+
+
+if __name__=="__main__":
+    from sklearn.svm import SVC
+
+    # Initialize the classifier, e.g., Support Vector Machine (SVC)
+    classifier = SVC(random_state=42)
+    model = Classifier(classifier=classifier)
+
+    # Fit the model on the data
+    #model.fit(X, y)
+
+    # Make predictions
+    #predictions = model.predict(X_test)
